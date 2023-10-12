d98ccdcfef33cd31e2d741697ff106240149dd15
mcdani978 <mcdani978@morrisville.edu>
2023-10-11 21:16:07 -0400

    Arrays

--- Quiz Master Name/Assets/Scripts/QuestionSO.cs
+++ Quiz Master Name/Assets/Scripts/QuestionSO.cs
@@ -7,10 +7,22 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string Str_question = "Enter new question text here";
+    [SerializeField] string[] Str_answers = new string[4];
+    [SerializeField] int Int_CorrectAnswerIndex;
 
     public string Str_GetQuestion()
     {
         return Str_question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return Str_answers[index];
+    }
+
+    public int Int_GetCorrectAnswerIndex()
+    { 
+        return Int_CorrectAnswerIndex;
     }
 }
 
